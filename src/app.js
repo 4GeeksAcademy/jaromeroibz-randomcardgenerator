e021ff7cc38173943a71264be9df1edf8deb16b6
jaromeroibz <137736111+jaromeroibz@users.noreply.github.com>
2023-08-24 01:25:19 +0000

    prueba

--- src/app.js
+++ src/app.js
@@ -5,7 +5,9 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function randomCard() {
+  document.getElementById("card").textContent =
+    Math.floor(Math.random() * 6) + 1;
+}
+
+randomCard();
