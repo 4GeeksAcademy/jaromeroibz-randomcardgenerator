a018e050a3f2da98f8b942fb625d461c34979d70
jaromeroibz <137736111+jaromeroibz@users.noreply.github.com>
2023-08-25 03:02:20 +0000

    proyecto finalizado

--- src/app.js
+++ src/app.js
@@ -5,9 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-function randomCard() {
-  document.getElementById("card").textContent =
-    Math.floor(Math.random() * 6) + 1;
+function cartaAleatoria() {
+  let numeroCarta = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  document.getElementById("carta").textContent =
+    numeroCarta[Math.floor(Math.random() * 13)];
+  let pintas = ["♦", "♥", "♠", "♣"];
+  let pintaRandom = pintas[Math.floor(Math.random() * 4)];
+  document.getElementById("pinta").textContent = pintaRandom;
+  document.getElementById("pinta2").textContent = pintaRandom;
+  document.querySelector(".pinta2").style.transform = "rotate(180deg)";
 }
 
-randomCard();
+cartaAleatoria();
