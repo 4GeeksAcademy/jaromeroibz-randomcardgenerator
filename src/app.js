f11d5270f425d00403a9eff38234fe3ea37c0955
jaromeroibz <137736111+jaromeroibz@users.noreply.github.com>
2023-08-26 03:23:18 +0000

    se incluyeron nuevas funciones

--- src/app.js
+++ src/app.js
@@ -5,7 +5,14 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-function cartaAleatoria() {
+window.onload = function() {
+  //write your code here
+  //console.log("Hello Rigo from the console!");
+  cartaAleatoria();
+  setInterval(cartaAleatoria, 10000);
+};
+
+window.cartaAleatoria = function cartaAleatoria() {
   let numeroCarta = [
     "A",
     "2",
@@ -21,13 +28,20 @@ function cartaAleatoria() {
     "Q",
     "K"
   ];
-  document.getElementById("carta").textContent =
+
+  document.getElementById("numerocarta").textContent =
     numeroCarta[Math.floor(Math.random() * 13)];
   let pintas = ["♦", "♥", "♠", "♣"];
   let pintaRandom = pintas[Math.floor(Math.random() * 4)];
   document.getElementById("pinta").textContent = pintaRandom;
   document.getElementById("pinta2").textContent = pintaRandom;
   document.querySelector(".pinta2").style.transform = "rotate(180deg)";
-}
+};
 
-cartaAleatoria();
+window.cambiarTamano = function cambiarTamano() {
+  console.log("cambio tamaño");
+  let alto = document.getElementById("alto").value;
+  let ancho = document.getElementById("ancho").value;
+  document.getElementById("carta").style.height = alto + "px";
+  document.getElementById("carta").style.width = ancho + "px";
+};
